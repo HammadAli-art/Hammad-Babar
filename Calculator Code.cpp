bee07fcc864f1f4b25408ee056459e3facb52ce0
Hammad Babar Ali <hammadbabar0326@gmail.com>
2025-02-21 09:58:40 -0800

    Add files via upload

---
+++ Calculator Code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int a, b;
+	cout<<"Enter the Value of a=";
+	cin>>a;
+	cout<<"Enter the Value of b=";
+	cin>>b;
+	cout<<"Sum="<<(a+b)<<endl;
+	cout<<"Difference="<<(a-b)<<endl;
+	cout<<"Division="<<(a/b)<<endl;
+	cout<<"Multiplication="<<(a*b)<<endl;
+	cout<<"Modulus="<<(a%b)<<endl;
+		return 0;
+	}
